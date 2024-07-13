2421c0eccbb0ccbca49fe2e7e6f92a70063688bf
twarowskiw <twojtek.ski@gmail.com>
2024-07-13 15:25:05 +0200

    Remove old frontend

---
+++ templates/src/app/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements OnInit {
+
+  private map!: L.Map;
+  private centroid: L.LatLngExpression = [52.230242538506374, 21.010196929725545] //Warsaw
+
+  initMap() {
+    this.map = L.map('map', {
+      zoomControl: false,
+      center: this.centroid,
+      zoom: 13,
+    });
+
+    const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      maxZoom: 19,
+	    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initMap()
+  }
+}
---
+++ templates/src/app/side-menu/side-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss'
+})
+export class SideMenuComponent {
+
+}
