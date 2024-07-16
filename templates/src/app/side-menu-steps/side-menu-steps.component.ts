1aa94249e3c29feb53a908883bf1215e434add47
twarowskiw <twojtek.ski@gmail.com>
2024-07-16 15:43:08 +0200

    Improve custom side-menu stepper

--- templates/src/app/side-menu-steps/side-menu-steps.component.ts
+++ templates/src/app/side-menu-steps/side-menu-steps.component.ts
@@ -1,20 +1,28 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
-import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu-steps',
   standalone: true,
-  imports: [MatStepperModule, RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule],
   templateUrl: './side-menu-steps.component.html',
-  styleUrl: './side-menu-steps.component.scss'
+  styleUrl: './side-menu-steps.component.scss',
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    },
+  ],
 })
 export class SideMenuStepsComponent {
+  currentRoute: string | undefined;
+  
   constructor(private router: Router) {}
 
-  onStepChange(event: any) {
-    const stepIndex = event.selectedIndex;
-    const routes = ['lines', 'settings', 'results'];
-    this.router.navigate([`/analyze/${routes[stepIndex]}`]);
+  ngOnInit() {
+    this.currentRoute = this.router.url;
   }
 }
