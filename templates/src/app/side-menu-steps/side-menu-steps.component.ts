d251fe06bf8df5624dd106f78a10bc061fca7c46
twarowskiw <twojtek.ski@gmail.com>
2024-07-16 22:25:28 +0200

    Improve stepper

--- templates/src/app/side-menu-steps/side-menu-steps.component.ts
+++ templates/src/app/side-menu-steps/side-menu-steps.component.ts
@@ -2,13 +2,13 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-side-menu-steps',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatButtonModule, CommonModule],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './side-menu-steps.component.html',
   styleUrl: './side-menu-steps.component.scss',
   providers: [
@@ -19,25 +19,5 @@ import { CommonModule } from '@angular/common';
   ],
 })
 export class SideMenuStepsComponent {
-  currentRoute: string | undefined;
-  
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    this.currentRoute = this.router.url;
-    console.log(this.currentRoute);
-  }
-
-  isActive(item: string) {
-    switch(item) {
-      case "lines":
-        return this.currentRoute?.includes("analyze/lines") ? "active-step" : "";
-      case "settings":
-        return /analyze\/\d+/.test(this.currentRoute!) == true ? "active-step" : ""; //fix this regex
-      case "results":
-        return this.currentRoute?.includes("analyze/results") ? "active-step" : "";
-      default:
-        return "";
-    }
-  }
+  constructor() {}
 }
