b9a7e3c91aad5bd377c0399873c86448fde3c81a
twarowskiw <twojtek.ski@gmail.com>
2024-07-16 10:47:18 +0200

    Change active area of the map responsively

--- templates/src/app/map/map.component.ts
+++ templates/src/app/map/map.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import 'leaflet-active-area';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
@@ -13,10 +16,21 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent implements OnInit {
 
-  private map!: L.Map;
-  private centroid: L.LatLngExpression = [52.2293364,20.958512] //Warsaw
+export class MapComponent implements OnInit {
+  /**
+   * Provide access to the DOM element
+   */
+  constructor(private elementRef: ElementRef<HTMLElement>) { }
+
+  /**
+   * Leaflet plugins don't have type definitions,
+   * so we need to define 'any' type (or write
+   * our own type definitions for needed functions).
+   */
+  private map!: any;
+  // private map!: L.Map;
+  private centroid: L.LatLngExpression = [52.2302, 21.0101] //Warsaw
 
   initMap() {
     this.map = L.map('map', {
@@ -25,9 +39,17 @@ export class MapComponent implements OnInit {
       zoom: 13,
     });
 
+    this.map.setActiveArea({
+      position: 'absolute',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      width: '66vw',
+    }, true, false);
+
     const tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
       maxZoom: 19,
-	    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map)
@@ -43,5 +65,35 @@ export class MapComponent implements OnInit {
 
   ngOnInit(): void {
     this.initMap()
+  }
+
+  /**
+   * When resized, set new active area of the map,
+   * but don't recenter, cause it works kind of weird
+   */
+  ngAfterViewInit() {
+    fromEvent(window, 'resize')
+      .pipe(debounceTime(200))
+      .subscribe(() => {
+        if(document.getElementById('main-component') != null) {
+          if (window.innerWidth > 1200) {
+            this.map.setActiveArea({
+              position: 'absolute',
+              top: '0',
+              right: '0',
+              bottom: '0',
+              width: (window.innerWidth - document.getElementById('main-component')!.offsetWidth) + 'px',
+            }, false, false);
+          } else {
+            this.map.setActiveArea({
+              position: 'absolute',
+              top: '0',
+              right: '0',
+              bottom: '0',
+              width: '100vw',
+            }, false, false);
+          }
+        }
+      });
   }
 }
