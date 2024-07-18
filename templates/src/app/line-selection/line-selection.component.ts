171b6d4d9b86fd22b9f25ba1d947f2977108939f
twarowskiw <twojtek.ski@gmail.com>
2024-07-18 16:05:51 +0200

    Add lines filtering and improve styles

--- templates/src/app/line-selection/line-selection.component.ts
+++ templates/src/app/line-selection/line-selection.component.ts
@@ -4,42 +4,71 @@ import { MatButtonModule } from '@angular/material/button';
 import { LinesService } from '../lines.service';
 import { CacheService } from '../cache.service';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-line-selection',
   standalone: true,
-  imports: [MatButtonModule, CommonModule],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './line-selection.component.html',
   styleUrl: './line-selection.component.scss',
   providers: []
 })
-export class LineSelectionComponent implements OnInit {
-  lines: any;
 
-  constructor(private router: Router, private linesService: LinesService, private cacheService: CacheService) {}
+export class LineSelectionComponent implements OnInit {
+  lines: Array<string>;
+  filteredLines: Array<string>;
+
+  constructor(private router: Router, private linesService: LinesService, private cacheService: CacheService) {
+    this.lines = [];
+    this.filteredLines = [];
+  }
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
   }
 
   getData() {
-    const cachedData = this.cacheService.get()
+    const cachedData = this.cacheService.get();
     if (!cachedData) {
-      this.linesService.getLines().subscribe(data => {
+      this.linesService.getLines().subscribe((data) => {
         try {
           this.cacheService.set(data);
           this.lines = data.lines;
-          console.log("fetch")
+          this.filteredLines = this.lines;
         } catch (error) {
           console.error(error);
         }
       });
     } else {
       this.lines = this.cacheService.get().lines;
+      this.filteredLines = this.lines;
     }
   }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+  }
+
+  // Add a trackBy function to improve performance when rendering the lines
+  trackByIndex(index: number, item: string) {
+    return index;
+  }
+
+  // Filter lines based on input value
+  applyFilter(filterValue: EventTarget) {
+    this.filteredLines = this.lines.filter((line) =>
+        line.toLowerCase().includes((filterValue as HTMLInputElement).value.toLowerCase())
+    );
+    if(this.filteredLines.length == 0)
+      this.filteredLines = this.lines;
   }
 }
