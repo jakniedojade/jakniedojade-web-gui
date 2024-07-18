8018c21359cd1671295d63c7d9be8bfc2181f5de
twarowskiw <twojtek.ski@gmail.com>
2024-07-18 20:32:15 +0200

    Add placeholder on empty menu pages

--- templates/src/app/app.routes.ts
+++ templates/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 import { LineSelectionComponent } from './line-selection/line-selection.component';
 import { SideMenuStepsComponent } from './side-menu-steps/side-menu-steps.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomeScreenComponent },
@@ -10,12 +11,12 @@ export const routes: Routes = [
         component: SideMenuStepsComponent,
         children: [
             { path: 'lines', component: LineSelectionComponent },
-            { path: ':line', component: WelcomeScreenComponent },
+            { path: ':line', component: PlaceholderComponent },
             { 
                 path: 'results', 
-                component: WelcomeScreenComponent,
+                component: PlaceholderComponent,
                 children: [
-                    { path: ':line', component: WelcomeScreenComponent },
+                    { path: ':line', component: PlaceholderComponent },
                 ]
             },
         ]
---
+++ templates/src/app/placeholder/placeholder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  standalone: true,
+  imports: [],
+  templateUrl: './placeholder.component.html',
+  styleUrl: './placeholder.component.scss'
+})
+export class PlaceholderComponent {
+
+}
