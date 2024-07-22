c8e51d9c59bb28f938dc0966e86d1846cdb8e81d
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-07-22 02:15:29 +0200

    Add material selects for stop selection

--- templates/src/app/stop-selection/stop-selection.component.ts
+++ templates/src/app/stop-selection/stop-selection.component.ts
@@ -3,17 +3,23 @@ import { StopsService } from '../stops.service';
 import { Observable } from 'rxjs';
 import { Stops } from '../stops';
 import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-stop-selection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSelectModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule],
   templateUrl: './stop-selection.component.html',
   styleUrl: './stop-selection.component.scss'
 })
 export class StopSelectionComponent {
   @Input() line!: string;
   line$!: Observable<Stops[]>;
+  //TODO use stop name variable in form control
+  panelStart = new FormControl('Szczęśliwice 05')
+  panelEnd = new FormControl('Pl. Piłsudskiego 06')
 
   constructor(private stopsService: StopsService) {}
 
