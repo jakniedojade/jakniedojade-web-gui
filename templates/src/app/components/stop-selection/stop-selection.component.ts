9eb14bc33e025aa96e071e218ec8beaca389ebb2
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-07-27 12:56:23 +0200

    Adjust stops request for new api

--- templates/src/app/components/stop-selection/stop-selection.component.ts
+++ templates/src/app/components/stop-selection/stop-selection.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { StopsService } from '../../services/stops.service';
 import { Observable } from 'rxjs';
-import { Stops } from '../../interfaces/stops';
+import { Stops, StopsInfo } from '../../interfaces/stops';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -15,21 +15,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './stop-selection.component.scss'
 })
 export class StopSelectionComponent {
-  @Input() line!: string;
-  stops$!: Observable<Stops[]>;
-  stops: Stops[];
+  @Input() line: string = "193"; //check if request works
+  stops$!: Observable<Stops>;
+  stopsInfo!: StopsInfo[];
   startIndex: number = 0;
   endIndex!: number;
   
   constructor(private stopsService: StopsService) {
-    this.stops = [];
+    this.stopsInfo = [];
   }
   
   ngOnInit(): void {
     this.stops$ = this.stopsService.getStops(this.line);
     this.stops$.subscribe((response) => {
-      this.stops = response;
-      this.endIndex = this.stops.length - 1;
+      this.stopsInfo = response.stops;
+      this.endIndex = this.stopsInfo.length - 1;
     })
   }
 }
--- templates/src/app/interfaces/stops.ts
+++ templates/src/app/interfaces/stops.ts
@@ -1,7 +1,14 @@
-export interface Stops {
+export interface StopsInfo {
     name: string;
     latitude: number;
     longitude: number;
     on_demand: boolean;
     id: string;
+}
+
+export interface Stops {
+    direction: number,
+    directionBeginning: string,
+    directionDestination: string,
+    stops: StopsInfo[];
 }
