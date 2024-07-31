3327efd264856a8ff19b7f8a729a0536e516c6bf
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-07-31 19:30:30 +0200

    Add results component

--- templates/src/app/components/stop-selection/stop-selection.component.ts
+++ templates/src/app/components/stop-selection/stop-selection.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { StopsService } from '../../services/stops.service';
 import { CacheService } from '../../services/cache.service';
-import { Stops, StopsInfo } from '../../interfaces/stops';
+import { StopsInfo } from '../../interfaces/stops';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,7 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-stop-selection',
