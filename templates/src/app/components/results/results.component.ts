2c6374a801548b4a52baf28b1aa16e934eeb19ed
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-08-02 01:47:20 +0200

    Add initial shapes handling in results component
    Currently drawing only one direction

--- templates/src/app/components/results/results.component.ts
+++ templates/src/app/components/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CacheService } from '../../services/cache.service';
+import { MapService } from '../../services/map.service';
 
 @Component({
   selector: 'app-results',
@@ -10,6 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultsComponent implements OnInit{
   private activatedRoute = inject(ActivatedRoute);
+  private cacheService = inject(CacheService);
+  private mapService = inject(MapService);
 
   public line: string = "";
 
@@ -17,5 +21,25 @@ export class ResultsComponent implements OnInit{
     this.activatedRoute.params.subscribe((lineParams: any) => {
       this.line = lineParams.line;
     })
+    this.getShapes(this.line, false); //TEMPORARY SET DIRECTION AS FALSE
+  }
+
+  getShapes(line: string, direction: boolean): void {
+    const shapes = this.cacheService.getCacheShapes(line, direction);
+
+    if (shapes && shapes.length > 0) {
+      const latLngArray = shapes.map(shape => ({
+        lat: shape.point_latitude,
+        lng: shape.point_longitude
+      }));
+  
+      this.plotOnMap(latLngArray);
+    } else {
+      console.log('No shapes found for the given line and direction.');
+    }
+  }
+  
+  plotOnMap(latLngArray: { lat: number; lng: number }[]): void {
+    this.mapService.drawRoute(latLngArray);
   }
 }
