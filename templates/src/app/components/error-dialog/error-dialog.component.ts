4102d1e467af20b9093ff504e51aedd77ed2af73
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-08-01 22:21:06 +0200

    Add error dialog component

---
+++ templates/src/app/components/error-dialog/error-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, Inject, Input } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogContent } from '@angular/material/dialog';
+import { MatDialogActions } from '@angular/material/dialog';
+import { MatDialogTitle } from '@angular/material/dialog';
+import { MatDialogClose } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-error-dialog',
+  standalone: true,
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogClose
+  ],
+  templateUrl: './error-dialog.component.html',
+  styleUrl: './error-dialog.component.scss'
+})
+export class ErrorDialogComponent {
+  data = inject<{ errorMessage: string }>(MAT_DIALOG_DATA);
+}
