cc0899fa287715a6f796cd1defe5f1802342e233
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-08-13 18:45:35 +0200

    Implement loading in side menu component

--- templates/src/app/components/side-menu/side-menu.component.ts
+++ templates/src/app/components/side-menu/side-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WelcomeScreenComponent } from '../welcome-screen/welcome-screen.component';
 import { RouterOutlet } from '@angular/router';
+import { LoadingIndicatorComponent } from '../loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [WelcomeScreenComponent, RouterOutlet],
+  imports: [WelcomeScreenComponent, RouterOutlet, LoadingIndicatorComponent],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss'
 })
