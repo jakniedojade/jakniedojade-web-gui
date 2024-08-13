591a45e1112028192244c8d6e9a62e3ce9b4702b
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-08-13 18:44:44 +0200

    Update app.config to handle http interceptor

--- templates/src/app/app.config.ts
+++ templates/src/app/app.config.ts
@@ -3,8 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './services/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(),
+    provideHttpClient(withInterceptors([loadingInterceptor]))
+  ]
 };
