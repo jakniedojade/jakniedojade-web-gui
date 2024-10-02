d2365d1870ff80110fe371bfa4990b684389d43a
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-02 15:08:39 +0200

    Add navigation buttons component

---
+++ templates/src/app/components/navigation-buttons/navigation-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-buttons',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-buttons.component.html',
+  styleUrl: './navigation-buttons.component.scss'
+})
+export class NavigationButtonsComponent {
+
+}
