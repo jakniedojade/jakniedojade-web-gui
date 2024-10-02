813e6ac4185c1f6a4e0c603a7903ec5d2dcad897
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-02 15:26:31 +0200

    Implement initial logic in navigation buttons component

--- templates/src/app/components/navigation-buttons/navigation-buttons.component.ts
+++ templates/src/app/components/navigation-buttons/navigation-buttons.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-navigation-buttons',
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: './navigation-buttons.component.html',
   styleUrl: './navigation-buttons.component.scss'
 })
 export class NavigationButtonsComponent {
+  @Input() nextDisabled: boolean = true;
 
+  @Output() previous = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();
+
+  onPrevious() {
+    this.previous.emit();
+  }
+
+  onNext() {
+    this.next.emit();
+  }
 }
