bbb082b7735948f9b7e6806a6b7b6a7b025b37b1
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-03 00:18:00 +0200

    Implement stops service logic
    To send requests to stops endpoint

--- templates/src/app/services/stops.service.ts
+++ templates/src/app/services/stops.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, shareReplay, throwError } from 'rxjs';
+import { Stops } from '../interfaces/stops';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StopsService {
+  private http = inject(HttpClient);
+  private stopsData$: Observable<Stops>;
 
-  constructor() { }
+  constructor() {
+    this.stopsData$ = this.http.get<Stops>('/api/v1/stops/')
+    .pipe(
+      shareReplay(),
+      catchError(this.handleError)
+    );
+  }
+
+  getStops() : Observable<Stops> {
+    return this.stopsData$;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('A client side or network error occurred:', error.error);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}`);
+    }
+    return throwError(() => new Error(`An error occurred while fetching stops`));
+  }
 }
