c28fafd09bceabf84a05dbdbbb7058853c27939d
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-04 18:58:47 +0200

    Add direction-selection component

---
+++ templates/src/app/components/direction-selection/direction-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direction-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './direction-selection.component.html',
+  styleUrl: './direction-selection.component.scss'
+})
+export class DirectionSelectionComponent {
+
+}
