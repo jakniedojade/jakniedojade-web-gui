fd20119bf29c79a27eea82cf991772c8b5c88cd3
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-05 18:57:55 +0200

    Add pole-selection component

---
+++ templates/src/app/components/pole-selection/pole-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pole-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './pole-selection.component.html',
+  styleUrl: './pole-selection.component.scss'
+})
+export class PoleSelectionComponent {
+
+}
