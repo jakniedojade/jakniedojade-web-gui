f7cd12de96cb1a35f2693842eac8cfdd00075318
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-05 18:45:20 +0200

    Implement direction-selection logic

--- templates/src/app/components/direction-selection/direction-selection.component.ts
+++ templates/src/app/components/direction-selection/direction-selection.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LineDataService } from '../../services/line-data.service';
+import { forkJoin } from 'rxjs';
+import { LineData, PolesDetails, Shapes } from '../../interfaces/line-data';
+import { ErrorDialogService } from '../../services/error-dialog.service';
+import { MatButton } from '@angular/material/button';
+import { NavigationButtonsComponent } from "../navigation-buttons/navigation-buttons.component";
+import { MapService } from '../../services/map.service';
 
 @Component({
   selector: 'app-direction-selection',
   standalone: true,
-  imports: [],
+  imports: [MatButton, NavigationButtonsComponent],
   templateUrl: './direction-selection.component.html',
   styleUrl: './direction-selection.component.scss'
 })
-export class DirectionSelectionComponent {
+export class DirectionSelectionComponent implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private lineDataService = inject(LineDataService);
+  private errorDialogService = inject(ErrorDialogService);
+  private mapService = inject(MapService);
+  private router = inject(Router);
+  
+  public line: string = "";
+  public selectedDirectionStopName: string = "";
+  public firstDirectionData: LineData | null = null;
+  public secondDirectionData: LineData | null = null;
 
+  public nextButtonDisabled = true;
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((lineParams: any) => {
+      this.line = lineParams.line;
+    });
+    this.fetchLineData();
+  }
+
+  private fetchLineData(): void {
+    forkJoin<[LineData, LineData]>([
+      this.lineDataService.getLineData(this.line, false),
+      this.lineDataService.getLineData(this.line, true),
+    ]).subscribe({
+      next: ([firstDirectionData, secondDirectionData]) => {
+        this.firstDirectionData = firstDirectionData;
+        this.secondDirectionData = secondDirectionData;
+      },
+      error: (err) => {
+        this.errorDialogService.openErrorDialog(err.message);
+      }
+    });
+  }
+
+  selectDirection(lineData: LineData) {
+    this.selectedDirectionStopName = lineData.endStopName;
+    this.nextButtonDisabled = false;
+    this.drawRouteAndPoles(lineData.shapes, lineData.poles)
+  }
+
+  private drawRouteAndPoles(shapes: Shapes[], poles: PolesDetails[]): void {
+    this.mapService.drawRoute(shapes);
+    this.mapService.drawPoles(poles);
+  }
+
+  navigateToLineAnalysisOptions(selectedDirection: string): void {
+    
+  }
+
+  navigateToLineSelection(): void {
+    this.router.navigate(["search"]);
+  }
 }
