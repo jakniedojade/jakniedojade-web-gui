10ba148d50c0c4cf18619bf8f59a12a2e973d43b
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-15 01:25:33 +0200

    Improve reactivity in pole-selection-component

--- src/app/components/pole-selection/pole-selection.component.ts
+++ src/app/components/pole-selection/pole-selection.component.ts
@@ -1,57 +1,48 @@
-import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PoleDetails } from '../../interfaces/line-data';
 import { ErrorDialogService } from '../../services/error-dialog.service';
 import { MatButton } from '@angular/material/button';
 import { NavigationButtonsComponent } from "../navigation-buttons/navigation-buttons.component";
 import { MapService } from '../../services/map.service';
 import { PolesOnStopService } from '../../services/poles-on-stop.service';
+import { catchError, Observable, of, switchMap, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-direction-selection',
   standalone: true,
-  imports: [MatButton, NavigationButtonsComponent],
+  imports: [MatButton, NavigationButtonsComponent, AsyncPipe],
   templateUrl: './pole-selection.component.html',
-  styleUrl: './pole-selection.component.scss'
+  styleUrl: './pole-selection.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PoleSelectionComponent implements OnInit, OnDestroy {
+export class PoleSelectionComponent {
   private errorDialogService = inject(ErrorDialogService);
   public mapService = inject(MapService);
   private router = inject(Router);
-  private polesOnStopService = inject(PolesOnStopService)
-  
-  @Input() routeStopId!: number;
-  @Input() routeStopName!: string;
+  private polesOnStopService = inject(PolesOnStopService);
+  private activatedRoute = inject(ActivatedRoute);
 
-  public poles: PoleDetails[] = [];
-
-  ngOnInit(): void {
-    this.fetchPolesOnStop();
-  }
-
-  ngOnDestroy(): void {
-    this.mapService.clearLayers();
-    this.mapService.resetMapView();
-  }
-
-  private fetchPolesOnStop(): void {
-    this.polesOnStopService.getPolesOnStop(this.routeStopId).subscribe({
-      next: (poles: PoleDetails[]) => {
-        this.poles = poles;
-        this.mapService.clearLayers();
-        this.mapService.drawPoles(this.poles);
-      },
-      error: (error) => {
-        this.errorDialogService.openErrorDialog(error.message);
-      }
-    });
-  }
+  polesOnStop$: Observable<PoleDetails[]> = this.activatedRoute.params.pipe(
+    switchMap(paramMap => this.polesOnStopService.getPolesOnStop(paramMap['routeStopId'])),
+    catchError(error => {
+      this.errorDialogService.openErrorDialog(error.message);
+      return of([]);
+    }),
+    tap(poles => {
+      this.mapService.clearLayers();
+      this.mapService.drawPoles(poles);
+    })
+  );
 
   navigateToPoleAnalysisOptions(): void {
     
   }
 
   navigateToLineSelection(): void {
+    this.mapService.clearLayers();
+    this.mapService.resetMapView();
     this.router.navigate(["search"]);
   }
 }
