cf7eba39a9fef3f2d47d6586db264bf9b44dbc54
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-16 18:14:40 +0200

    Add pole and direction analysis type selection components

---
+++ src/app/components/pole-analysis-selection/pole-analysis-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pole-analysis-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './pole-analysis-selection.component.html',
+  styleUrl: './pole-analysis-selection.component.scss'
+})
+export class PoleAnalysisSelectionComponent {
+
+}
