1a119d543fd6a313da45903308e92d259a102d17
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-18 13:19:40 +0200

    Add lines on pole selection component

---
+++ src/app/components/lines-on-pole-selection/lines-on-pole-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lines-on-pole-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './lines-on-pole-selection.component.html',
+  styleUrl: './lines-on-pole-selection.component.scss'
+})
+export class LinesOnPoleSelectionComponent {
+
+}
