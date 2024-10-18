99d48d33ee3c0dd5e88c93a8cde79d942c69b457
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-18 13:14:44 +0200

    Add lines on pole service

---
+++ src/app/services/lines-on-pole.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, shareReplay, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinesOnPoleService {
+  private http = inject(HttpClient);
+
+  getLinesOnPole(poleId: number): Observable<string[]> {
+    return this.http.get<string[]>(`/api/v1/poles/${poleId}/lines/`)
+      .pipe(
+        shareReplay(),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('A client-side or network error occurred:', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}`);
+    }
+    return throwError(() => new Error('An error occurred while fetching lines on pole'));
+  }
+}
