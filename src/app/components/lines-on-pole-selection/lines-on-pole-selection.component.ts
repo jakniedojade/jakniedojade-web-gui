fe76b2bcc23feb9af151babde62f0ddc7c19def2
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-10-30 15:31:39 +0100

    Implement line icons in lines-on-pole component

--- src/app/components/lines-on-pole-selection/lines-on-pole-selection.component.ts
+++ src/app/components/lines-on-pole-selection/lines-on-pole-selection.component.ts
@@ -4,7 +4,7 @@ import { ErrorDialogService } from '../../services/error-dialog.service';
 import { map } from 'rxjs/operators';
 import { MapService } from '../../services/map.service';
 import { PolesOnStopService } from '../../services/poles-on-stop.service';
-import { catchError, filter, of, switchMap, tap } from 'rxjs';
+import { catchError, combineLatest, filter, forkJoin, of, switchMap, tap } from 'rxjs';
 import { PoleDetails } from '../../interfaces/line-data';
 import { LinesOnPoleService } from '../../services/lines-on-pole.service';
 import { AsyncPipe } from '@angular/common';
@@ -12,6 +12,8 @@ import { MapComponent } from "../map/map.component";
 import { NavigationButtonsComponent } from "../navigation-buttons/navigation-buttons.component";
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
+import { LinesService } from '../../services/lines.service';
+import { LineDataService } from '../../services/line-data.service';
 
 @Component({
   selector: 'app-lines-on-pole-selection',
@@ -27,6 +29,7 @@ export class LinesOnPoleSelectionComponent {
   private polesOnStopService = inject(PolesOnStopService);
   private linesOnPoleService = inject(LinesOnPoleService);
   private activatedRoute = inject(ActivatedRoute);
+  private linesService = inject(LinesService);
 
   lineSelection = signal<string>('');
 
@@ -49,7 +52,19 @@ export class LinesOnPoleSelectionComponent {
   );
 
   linesOnPole$ = this.selectedPoleFromRoute$.pipe(
-    switchMap(pole => this.linesOnPoleService.getLinesOnPole(pole!.id)),
+    switchMap(pole => 
+      this.linesOnPoleService.getLinesOnPole(pole!.id).pipe(
+        switchMap(lines => 
+          forkJoin(
+            lines.map(line => 
+              this.linesService.getLineIcon(line).pipe(
+                map(icon => ({ lineNumber: line, icon }))
+              )
+            )
+          )
+        )
+      )
+    ),
     catchError(error => {
       this.errorDialogService.openErrorDialog(error.message);
       return of(null);
