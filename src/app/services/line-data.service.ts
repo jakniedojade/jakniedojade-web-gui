72d4211d10ab060a95938d955b35df02a4d124cc
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-11-02 17:28:56 +0100

    Add error handling for empty poles array in LineData

--- src/app/services/line-data.service.ts
+++ src/app/services/line-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, shareReplay, throwError } from 'rxjs';
+import { catchError, map, Observable, shareReplay, throwError } from 'rxjs';
 import { LineData } from '../interfaces/line-data';
 
 @Injectable({
@@ -14,24 +14,35 @@ export class LineDataService {
     const directionNumber: string = directionSwapped ? "1" : "0";
     const url = `/api/v1/lines/${line}/poles/?direction=${directionNumber}`;
     const key = this.constructKey(line, directionSwapped);
+
     if (!this.lineData.has(key)) {
-      this.lineData.set(key, this.http.get<LineData>(url)
-      .pipe(
+      this.lineData.set(key, this.http.get<LineData>(url).pipe(
+        map(lineData => {
+          if (!lineData.poles || lineData.poles.length === 0) {
+            throw new Error('No poles data available');
+          }
+          return lineData;
+        }),
         shareReplay(),
-        catchError(this.handleError)
+        catchError(error => this.handleError(error))
       ));
     }
+
     return this.lineData.get(key) as Observable<LineData>;
   }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('A client side or network error occurred:', error.error);
+  private handleError(error: Error) {
+    if (error instanceof HttpErrorResponse) {
+      if (error.status === 0) {
+        console.error('A client side or network error occurred:', error.error);
+      } else {
+        console.error(`Backend returned code ${error.status}`);
+      }
+      return throwError(() => new Error('An error occurred while fetching stops'));
     } else {
-      console.error(
-        `Backend returned code ${error.status}`);
+      console.error(error.message);
+      return throwError(() => new Error(error.message));
     }
-    return throwError(() => new Error(`An error occurred while fetching stops`));
   }
 
   private constructKey(line: string, directionSwapped: boolean): string {
