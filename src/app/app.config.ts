90a302abef4aa3861d325bb99f78a24b31df16e0
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-11-05 23:08:09 +0100

    Enable hashLocation routing
    To prevent 404 on refresh

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,7 +7,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from './services/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding() ), provideAnimationsAsync(), provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation(), withComponentInputBinding() ), provideAnimationsAsync(), provideHttpClient(),
     provideHttpClient(withInterceptors([loadingInterceptor])), provideAnimationsAsync()
   ]
 };
